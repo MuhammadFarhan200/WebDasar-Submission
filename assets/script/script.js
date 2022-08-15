4ddfb19973312af9ff8d7dbe55c08937f90ebf1d
Abdullahgybran123 <abdullahgybranr@gmail.com>
2022-08-15 18:13:58 +0700

    Submission Kelas Web Dasar Dicoding Academy

---
+++ assets/script/script.js
@@ -0,0 +1,34 @@
+function responsive() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className += " show_nav";
+    } else {
+        x.className = "nav";
+    }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slider");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
